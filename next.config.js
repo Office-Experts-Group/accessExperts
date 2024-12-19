ae7bff8a1df15472001a9748f3b69d7ffb6469e3
Gitit2meBaby <thisworldofdans@gmail.com>
2024-12-19 16:57:51 +0700

    reverting more and more

--- next.config.js
+++ next.config.js
@@ -4,42 +4,9 @@ const nextConfig = {
   poweredByHeader: false,
   reactStrictMode: true,
 
-  // Optimize image handling
   images: {
     formats: ["image/webp"],
-    deviceSizes: [640, 750, 828, 1080, 1200],
-    imageSizes: [16, 32, 48, 64, 96],
     minimumCacheTTL: 31536000,
-  },
-
-  // Add production optimizations
-  webpack: (config, { dev, isServer }) => {
-    // Production optimizations only
-    if (!dev && !isServer) {
-      config.optimization.splitChunks = {
-        chunks: "all",
-        minSize: 20000,
-        maxSize: 70000,
-        cacheGroups: {
-          default: false,
-          vendors: false,
-          framework: {
-            chunks: "all",
-            name: "framework",
-            test: /(?<!node_modules.*)[\\/]node_modules[\\/](react|react-dom|scheduler|prop-types|use-subscription)[\\/]/,
-            priority: 40,
-            enforce: true,
-          },
-          commons: {
-            name: "commons",
-            chunks: "all",
-            minChunks: 2,
-            priority: 20,
-          },
-        },
-      };
-    }
-    return config;
   },
 };
 
