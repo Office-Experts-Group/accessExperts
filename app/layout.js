65c279803452f32afdf344078b4f3505733d7bf9
Gitit2meBaby <thisworldofdans@gmail.com>
2024-12-19 19:27:21 +0700

    Revert "removed interaction observer, reverted to scroll based animation"
    This reverts commit 0071711cce690a163a52325d6d6d3f12009e800c.

--- app/layout.js
+++ app/layout.js
@@ -128,9 +128,21 @@ export const metadata = {
 
   // Icons
   icons: {
-    icon: "/favicon.ico",
+    icon: [
+      { url: "/favicon.ico", sizes: "32x32" },
+      { url: "/favicon.ico", sizes: "16x16" },
+    ],
     shortcut: "/favicon-32x32.png",
     apple: "/apple-touch-icon.png",
+  },
+  other: {
+    link: [
+      {
+        rel: "preload",
+        href: "/hero/handShake.webp",
+        as: "image",
+      },
+    ],
   },
 };
 
