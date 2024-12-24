e8902a31ba1170b489585c1650c080322a94f217
Gitit2meBaby <thisworldofdans@gmail.com>
2024-12-24 11:22:51 +0700

    config mirrors other sites

--- next.config.js
+++ next.config.js
@@ -1,59 +1,13 @@
 /** @type {import('next').NextConfig} */
-const path = require("path");
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-
 const nextConfig = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
-  },
-  images: {
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    formats: ["image/webp"],
-  },
-  // Production optimizations
-  productionBrowserSourceMaps: false,
-  poweredByHeader: false,
   compress: true,
-  cssChunking: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
 
-  webpack: (config, { dev, isServer }) => {
-    if (!dev) {
-      // Production optimizations
-      config.optimization = {
-        ...config.optimization,
-        moduleIds: "deterministic",
-        splitChunks: {
-          chunks: "all",
-          maxInitialRequests: 25,
-          minSize: 20000,
-          cacheGroups: {
-            default: false,
-            vendors: false,
-            // Combine React framework chunks
-            framework: {
-              name: "framework",
-              chunks: "all",
-              test: /[\\/]node_modules[\\/](react|react-dom|scheduler|next)[\\/]/,
-              priority: 40,
-              enforce: true,
-              reuseExistingChunk: true,
-            },
-            commons: {
-              name: "commons",
-              minChunks: 2,
-              priority: 20,
-              reuseExistingChunk: true,
-            },
-          },
-        },
-      };
-    }
-
-    return config;
+  images: {
+    formats: ["image/webp"],
+    minimumCacheTTL: 31536000,
   },
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = nextConfig;
